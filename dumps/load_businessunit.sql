2c7cb83485f111d495c1f0e648ed213fd8a62464
karwootang-gft <karwoo.tang@gft.com>
2020-07-20 16:26:08 +0100

    wip

--- dumps/load_businessunit.sql
+++ dumps/load_businessunit.sql
@@ -2,7 +2,7 @@ LOCK TABLES `businessunit` WRITE;
 INSERT INTO `businessunit` VALUES 
 (1,'Modern Apps','Modern Apps',1),
 (2,'Data','Data',1),
-(3,'FICC','FICC',1);
-(4,'Finance','Finance',1);
+(3,'FICC','FICC',1),
+(4,'Finance','Finance',1),
 (5,'HR','HR',1);
 UNLOCK TABLES;
