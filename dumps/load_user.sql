958dbbcd325948e342b35470aaef29b8eb2f0a48
karwootang-gft <karwoo.tang@gft.com>
2020-07-20 18:42:05 +0100

    wip

--- dumps/load_user.sql
+++ dumps/load_user.sql
@@ -1,8 +1,8 @@
 LOCK TABLES `user` WRITE;
 INSERT INTO `user` VALUES 
-(1,'dev@your.company','Jon','Snow',0,"2020-03-01 12:34:56",1,1),
-(2,'admin@your.company','Adam','Smith',1,"2020-03-01 12:34:56",1,1),
-(3,'awrr@tranquilitybase-demo.io','Andrew','Rossiter',1,"2020-07-20 15:50:00",1,1),
-(4,'john.doe@tranquilitybase-demo.io','John','Doe',0,"2020-07-20 11:15:26",1,1),
-(5,'jane.doe@tranquilitybase-demo.io','Jane','Doe',0,"2020-07-20 11:16:26",1,1);
+(1,'dev@your.company','Jon','Snow',0,"2020-03-01 12:34:56",1,1,'$2b$12$Rcwh92zNpTcQZCnMyBbMNuPIjCE2NdgqFli0fGQlNbLcTKRg5YQz.'),
+(2,'admin@your.company','Adam','Smith',1,"2020-03-01 12:34:56",1,1,'$2b$12$Rcwh92zNpTcQZCnMyBbMNuPIjCE2NdgqFli0fGQlNbLcTKRg5YQz.'),
+(3,'awrr@tranquilitybase-demo.io','Andrew','Rossiter',1,"2020-07-20 15:50:00",1,1,'$2b$12$Rcwh92zNpTcQZCnMyBbMNuPIjCE2NdgqFli0fGQlNbLcTKRg5YQz.'),
+(4,'john.doe@tranquilitybase-demo.io','John','Doe',0,"2020-07-20 11:15:26",1,1'$2b$12$Rcwh92zNpTcQZCnMyBbMNuPIjCE2NdgqFli0fGQlNbLcTKRg5YQz.'),
+(5,'jane.doe@tranquilitybase-demo.io','Jane','Doe',0,"2020-07-20 11:16:26",1,1,'$2b$12$Rcwh92zNpTcQZCnMyBbMNuPIjCE2NdgqFli0fGQlNbLcTKRg5YQz.');
 UNLOCK TABLES;
--- tables/user.sql
+++ tables/user.sql
@@ -16,6 +16,7 @@ CREATE TABLE `user` (
   `lastUpdated` datetime DEFAULT NULL,
   `isActive` tinyint(1) NOT NULL DEFAULT '0',
   `showWelcome` tinyint(1) DEFAULT '1',
+  `hash` char(60) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
