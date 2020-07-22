02221094c533951e7653ae0ab3e0c027e84f72cc
karwootang-gft <karwoo.tang@gft.com>
2020-07-22 09:45:23 +0100

    revert user table

--- tables/user.sql
+++ tables/user.sql
@@ -15,7 +15,7 @@ CREATE TABLE `user` (
   `isAdmin` tinyint(1) NOT NULL DEFAULT '0',
   `lastUpdated` datetime DEFAULT NULL,
   `isActive` tinyint(1) NOT NULL DEFAULT '0',
-  `showWelcome` tinyint(1) DEFAULT '1'
+  `showWelcome` tinyint(1) DEFAULT '1',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 /*!40101 SET character_set_client = @saved_cs_client */;
