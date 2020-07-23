7bd9963b60363ade1d1f1f7ddaa0e17396432347
karwootang-gft <karwoo.tang@gft.com>
2020-07-23 21:51:36 +0100

    add tables

---
+++ tables/notificationSolutionDeployment.sql
@@ -0,0 +1,17 @@
+SET FOREIGN_KEY_CHECKS=0;
+
+DROP TABLE IF EXISTS `notificationSolutionDeployment`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `notificationSolutionDeployment` (
+  `notificationId` int(11) NOT NULL,
+  `solutionId` int(11) NOT NULL,
+  `isActive` tinyint(1) NOT NULL DEFAULT '1',
+  `lastUpdated` datetime DEFAULT NULL,
+  PRIMARY KEY (`notificationId`, `teamId`),
+  CONSTRAINT `FK_notificationSolutionDeployment_notificationId` FOREIGN KEY (`notificationId`) REFERENCES `notification` (`id`),  
+  CONSTRAINT `FK_notificationSolutionDeployment_solutionId` FOREIGN KEY (`solutionId`) REFERENCES `solution` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+SET FOREIGN_KEY_CHECKS=1;
