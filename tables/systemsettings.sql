d97105410e66ac5c057695664aa04f159bfb0aca
aivo <aivo@gft.com>
2020-12-08 11:19:00 +0000

    update userid field

--- tables/systemsettings.sql
+++ tables/systemsettings.sql
@@ -8,14 +8,14 @@ DROP TABLE IF EXISTS `systemsettings`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `systemsettings` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userID` int FOREIGN KEY PREFERENCES USER(id),
+  `userId` int FOREIGN KEY PREFERENCES USER(id),
   `username` varbinary(255),
   `token` varbinary(255),
 
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`),
   KEY `FK_user_Id` (`id`),
-  CONSTRAINT `FK_user_id` FOREIGN KEY (`userID`) REFERENCES `user` (`id`),
+  CONSTRAINT `FK_user_id` FOREIGN KEY (`userId`) REFERENCES `user` (`id`),
 ) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
